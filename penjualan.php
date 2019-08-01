bdf25612d753fe366ed68751cc2ae2796cd74097
selinurfitriyani <selinurfitriyani28@gmail.com>
2019-08-01 17:00:01 +0700

    umur dan perdagangan

---
+++ penjualan.php
@@ -0,0 +1,8 @@
+<?php
+$harga = 15000;
+$dus = 6;
+$isi = 5;
+$a = 6*2000;
+$perhitungan = ($harga*$dus*$isi)+$a;
+echo "Hasilnya adalah : $perhitungan";
+?>
---
+++ umur.php
@@ -0,0 +1,16 @@
+<?php
+$Tama = 1;
+$Toni = 0.5;
+$jtiniTama = 14;
+$jtiniToni = 34;
+
+echo"Umur tama = $jTama X<br>";
+echo"Umur toni = $jToni X<br>";
+echo"Umur toni + umur tini = $jtiniToni<br>";
+echo"$jtoni + $jtamaX - $jtiniTama= $jtiniToni<br>";
+$x =$jTama + $Toni;
+$y =$jtiniTama + $jtiniToni;
+$z = $y/$x;
+echo "$x X = $jtiniTama + $jtiniToni<br>";
+echo "$x X = $y<br>";
+
