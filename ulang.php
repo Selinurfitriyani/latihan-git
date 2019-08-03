4e593a1e9a97b972e367197d1a00c1423de66d0a
selinurfitriyani <selinurfitriyani28@gmail.com>
2019-08-03 16:07:59 +0700

    Tentang array

---
+++ ulang.php
@@ -0,0 +1,7 @@
+<?php
+$b = 'Fitri';
+$a = [1,2,4,300,400,700,300,'Helo','Seli',$b,'Zeyenk'];
+for ($i=0; $i < count($a) ; $i++) { 
+    echo 'Index array ke-'.$i.'Dengan isi data '.$a[$i] . '<br>';
+}
+?>
